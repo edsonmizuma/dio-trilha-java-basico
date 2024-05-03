94d067739fd2ba02152a5180a555b98a7795132c
edson mizuma <edson.mizuma@gmail.com>
2024-05-03 11:21:58 -0300

    first commit of smart tv

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        boolean condicao1 = false;
+
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        } else if (condicao1 || condicao2) {
+            System.out.println("umas das condições são verdadeiras");
+        } else {
+            System.out.println("Ambas são falsas");
+        }
     }
 }
