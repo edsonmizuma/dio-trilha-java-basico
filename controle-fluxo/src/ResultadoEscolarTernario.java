7116dfe9ad1f40c70e76c7895ee8e1c14bf35a76
edson mizuma <edson.mizuma@gmail.com>
2024-05-08 09:51:52 -0300

    alteração do código com inclusão do if else

--- controle-fluxo/src/ResultadoEscolarTernario.java
+++ controle-fluxo/src/ResultadoEscolarTernario.java
@@ -1,8 +1,8 @@
 public class ResultadoEscolarTernario {
 
     public static void main(String[] args) {
-        int nota = 3;
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        int nota = 2;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
         System.out.println(resultado);
     }
 
